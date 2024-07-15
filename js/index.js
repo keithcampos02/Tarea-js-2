3d6a3ba90407b426c6b48a7085c14e648a1af10b
keithcampos02 <alonso826campos@gmail.com>
2024-07-14 18:28:40 -0600

    feat primera parte

---
+++ js/index.js
@@ -0,0 +1,33 @@
+console.log("funcioanndo")
+
+
+function enviarSolicitud(event) {
+    event.preventDefault()
+const formulario = document.getElementById("formularioSolicitud");
+const nombre = document.getElementById("nombre").value;
+const email = document.getElementById("email").value;
+const asunto = document.getElementById("asunto").value;
+const mensaje = document.getElementById("mensaje").value;
+let copia = document.getElementById("copia").value;
+
+switch (copia) {
+    case "si":
+        alert("se enviara copia de solciitud al correo")
+        break;
+        case "no":
+        alert(" no se enviara ninguna copia")
+        break;
+
+    default:
+        break;
+}
+
+
+
+resultado.textContent=`El nombres es ${nombre} el correo es  ${email} el asunto es  ${asunto} el mensaje es  ${mensaje}`
+
+    
+}
+
+let captura = copia;
+
